01892c2da3cdabf8fc85dfec07cd5e48bdbac6f7
DEVANSH <devansh.22063@iitgoa.ac.in>
2025-10-03 11:30:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/devansh-portfolio/', // must match repo exactly
+  base: '/devansh-portfolio/', // must match your repo name, keep the trailing slash
 })
